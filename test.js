0fc409279720439e5321072c756d92fdd3c31b5c
jonschlinkert <jon.schlinkert@sellside.com>
2015-03-07 23:20:01 -0500

    move

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 /*!
  * falsey <https://github.com/jonschlinkert/falsey>
  *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
+ * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
 
 var assert = require('assert');
-var isFalsey = require('../');
+var isFalsey = require('./');
 
 
 describe('falsey', function () {
@@ -20,6 +20,7 @@ describe('falsey', function () {
     assert(isFalsey(0));
     assert(!isFalsey(1));
     assert(isFalsey(''));
+    assert(isFalsey('0'));
     assert(!isFalsey('1'));
     assert(isFalsey(NaN));
     assert(isFalsey({}));
@@ -27,6 +28,7 @@ describe('falsey', function () {
     assert(isFalsey([]));
     assert(!isFalsey([0]));
 
+    // arguments
     (function(){
       assert(isFalsey(arguments));
     }());
@@ -43,14 +45,11 @@ describe('falsey', function () {
       assert(!isFalsey(args));
     }(0));
 
-    assert(isFalsey('false'));
-    assert(!isFalsey('false', [])); // override special cases
     assert(!isFalsey('true'));
+    // custom values
     assert(isFalsey('nil'));
     assert(!isFalsey('nil', []));
     assert(isFalsey('none'));
     assert(!isFalsey('none', []));
-    assert(isFalsey('null'));
-    assert(!isFalsey('null', []));
   });
 });
