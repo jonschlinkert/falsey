5136ef1c011fa97ae3df5b9d82a54d9ca7694bfc
doowb <brian.woodward@gmail.com>
2017-09-11 15:01:33 -0400

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * falsey <https://github.com/jonschlinkert/falsey>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
- * Licensed under the MIT License.
+ * Copyright (c) 2014-2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /*!
  * falsey <https://github.com/jonschlinkert/falsey>
  *
- * Copyright (c) 2014-2015, Jon Schlinkert.
+ * Copyright (c) 2014-2017, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
